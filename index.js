84dbf421314ad28bf86d1c0ab6dcd89b736a17f7
aldehf420 <aldehf420@naver.com>
2019-10-07 23:40:05 +0900

    link_db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var engine = require('consolidate');
 var express = require('express');
-// var mysql = require('mysql');
+var mysql = require('mysql');
 var app = express();
 
 
@@ -51,7 +51,15 @@ app.get('/research',function(req,res){
     res.render('research.html');
 });
 
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
 
+	post: 3306,
+
+	password: '1234',
+	database: 'my_db'
+});
 
 
 
