c3e80144ebdf9ac7cfbe9eaf44c6592fc2a30424
aldehf420 <aldehf420@naver.com>
2019-10-14 00:28:10 +0900

    merge

--- public/js/template.js
+++ public/js/template.js
@@ -1,15 +1,15 @@
 module.exports = {
-  HTML : function (title,list,feature) {
-      return `
+    HTML: function(title, list, feature) {
+        return `
       <!DOCTYPE html>
       <html>
-      <head>
+      <head >
           <meta charset="utf-8" />
           <link href="css/main.css?ver=1" type="text/css" rel="stylesheet" />
       </head>
 
-      <body>
-          <h1><a href="">This is ${title}</a></h1>
+      <body class="test">
+          <h1 class = "test"><a href="">This is ${title}</a></h1>
           ${list}
           <a href="?id=login">LOGIN</a>
           <a href="?id=contact">CONTACT</a>
@@ -17,15 +17,16 @@ module.exports = {
       </body>
       </html>
       `;
-  },list: function (filelist) {
-      var list = '<ul>';
-      var i = 0;
-      while(i < filelist.length){
-          var title = filelist[i].split('.')[0];
-          list = list + `<li><a href="/?id=${title}">${title}</a></li>`;
-          i = i + 1;
-      }
-      list = list+'</ul>';
-      return list;
-  }
+    },
+    list: function(filelist) {
+        var list = '<nav id="topMenu" ><ul>';
+        var i = 0;
+        while (i < filelist.length) {
+            var title = filelist[i].split('.')[0];
+            list = list + `<li><a class="menuLink" href="/?id=${title}">${title}</a></li>`;
+            i = i + 1;
+        }
+        list = list + '</ul></nav>';
+        return list;
+    }
 }
