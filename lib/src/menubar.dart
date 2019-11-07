9bae96aee0cbaf42d86b6f632fd6f16192ffa85e
KimDongHyun <kdh2637@hanyang.ac.kr>
2019-11-07 21:08:59 +0900

    add course2 which is graduateSchoolCourse

--- lib/src/menubar.dart
+++ lib/src/menubar.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:untitled3/src/course2.dart';
 import 'package:untitled3/src/login.dart';
 import 'package:untitled3/src/notice.dart';
 import 'package:untitled3/src/startpage.dart';
 import 'package:untitled3/src/testlogin.dart';
 import 'package:untitled3/src/members.dart';
-
-import 'member.dart';
 
 
 class MenuForGraduate extends StatelessWidget {
@@ -48,11 +47,7 @@ class MenuForGraduate extends StatelessWidget {
             title: Text('Members',textScaleFactor: 1.5,),
             onTap: () {
               Navigator.of(context)
-<<<<<<< Updated upstream
                   .push(MaterialPageRoute(builder: (context) => Members()));
-=======
-                  .push(MaterialPageRoute(builder: (context) => Member()));
->>>>>>> Stashed changes
             },
           ),
           Divider(),
@@ -80,7 +75,8 @@ class MenuForGraduate extends StatelessWidget {
           ListTile(
             title: Text('Courses',textScaleFactor: 1.5,),
             onTap: () {
-              Navigator.pop(context);
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => CoursePage2()));
             },
           ),
           Divider(),
