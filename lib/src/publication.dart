4909d04e3080e25c33dd9c7697eb2b82cc33936c
hsh04060 <hsh04060@naver.com>
2019-11-09 00:23:46 +0900

    publication

--- lib/src/publication.dart
+++ lib/src/publication.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:expandable/expandable.dart';
@@ -9,29 +10,32 @@ class Publication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text('Publications')
-        ),
-        body: StreamBuilder<QuerySnapshot>(
-          stream: Firestore.instance.collection('publications').snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return const Text('Loading');
-            return ListView.builder(
-                //itemCount: snapshot.data.documents.length,
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: <Widget>[
-                      _buildListItem(context, snapshot.data.documents[index]),
-                      Divider(
-                        height: 10,
-                        color: Colors.white,
-                      )
-                    ],
-                  );
-                },
-            );
-          },
-        )
+      appBar: AppBar(
+        title: Text('Publications'),
+      ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('publications')
+            .orderBy('category', descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const Text('Loading...');
+          return ListView.builder(
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: <Widget>[
+                  _buildListItem(context, snapshot.data.documents[index]),
+                  Divider(
+                    height: 10,
+                    color: Colors.white,
+                  )
+                ],
+              );
+            },
+          );
+        },
+      ),
     );
   }
 
@@ -52,16 +56,31 @@ class Publication extends StatelessWidget {
           textDirection: TextDirection.ltr,
         ),
       ),
-      expanded: Container(
-        padding: EdgeInsets.only(top: 10),
-        child: Text(
-          document['In Proc.'],
-          softWrap: true,
-          textScaleFactor: 2,
-        ),
+      expanded: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              document['In Proc.'],
+              softWrap: true,
+              textScaleFactor: 1.5,
+            ),
+          ),
+          Container(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      document['pdf'].toString(),
+                      textScaleFactor: 1.5,
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                ],
+              ))
+        ],
       ),
       tapHeaderToExpand: true,
-      hasIcon: true,
     );
   }
 }
