93d6e35a71453936961aa4415a7b29af45d2cd8a
gohdong <aldehf420@naver.com>
2019-11-09 13:06:25 +0900

    merge

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/gohdong/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/gohdong/IdeaProjects/selab"
-export "FLUTTER_TARGET=/Users/gohdong/IdeaProjects/selab/lib/main.dart"
+export "FLUTTER_ROOT=C:\Users\aldeh\Development\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\aldeh\Workspace\2019_autumn_webapp"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/gohdong/development/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\aldeh\Development\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
