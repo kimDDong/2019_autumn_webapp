f45d76cc3a2d6b78d92ad23dc0ed873945aa6820
hsh04060 <hsh04060@naver.com>
2019-11-09 12:16:49 +0900

    #45 publication

--- lib/src/publication.dart
+++ lib/src/publication.dart
@@ -1,86 +1,87 @@
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:expandable/expandable.dart';
-
+import 'package:untitled3/src/showpub.dart';
 
 class Publication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Publications'),
+        title: Text("Publications"),
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection('publications')
-            .orderBy('category', descending: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) return const Text('Loading...');
-          return ListView.builder(
-            itemCount: snapshot.data.documents.length,
-            itemBuilder: (context, index) {
-              return Column(
-                children: <Widget>[
-                  _buildListItem(context, snapshot.data.documents[index]),
-                  Divider(
-                    height: 10,
-                    color: Colors.white,
-                  )
-                ],
-              );
-            },
-          );
-        },
-      ),
+      body: BodyLayout(),
     );
   }
+}
 
-  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
-    return ExpandablePanel(
-      header: Text(
-        document['title'],
-        textScaleFactor: 1.5,
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-      collapsed: Align(
-        alignment: Alignment.centerLeft,
-        child: Text(
-          document['writer'],
-          softWrap: true,
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          textDirection: TextDirection.ltr,
+class BodyLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView(context);
+  }
+}
+
+Widget _myListView(BuildContext context) {
+  final Size size = MediaQuery
+      .of(context)
+      .size;
+  return ListView(
+    children: <Widget>[
+      Container(
+        height: size.height * 0.1,
+        child: ListTile(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute<void>(builder: (BuildContext context) => ShowPublication(category : 'International Conference'))
+            );
+          },
+          title: Text('International Conference', textScaleFactor: 1.3,),
         ),
       ),
-      expanded: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(15),
-            child: Text(
-              document['In Proc.'],
-              softWrap: true,
-              textScaleFactor: 1.5,
-            ),
-          ),
-          Container(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      document['pdf'].toString(),
-                      textScaleFactor: 1.5,
-                      style: TextStyle(fontStyle: FontStyle.italic),
-                    ),
-                  ),
-                ],
-              ))
-        ],
+      Divider(
+          height: 10,
+          color: Colors.white
       ),
-      tapHeaderToExpand: true,
-    );
-  }
+      Container(
+        height: size.height * 0.1,
+        child: ListTile(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute<void>(builder: (BuildContext context) => ShowPublication(category : 'International Journal'))
+            );
+          },
+          title: Text('International Journal', textScaleFactor: 1.3),
+        ),
+      ),
+      Divider(
+          height: 10,
+          color: Colors.white
+      ),
+      Container(
+        height: size.height * 0.1,
+        child: ListTile(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute<void>(builder: (BuildContext context) => ShowPublication(category : 'Domestic Conference'))
+            );
+          },
+          title: Text('Domestic Conference', textScaleFactor: 1.3),
+        ),
+      ),
+      Divider(
+        height: 10,
+        color: Colors.white,
+      ),
+      Container(
+        height: size.height * 0.1,
+        child: ListTile(
+          onTap: (){
+            Navigator.push(context,
+                MaterialPageRoute<void>(builder: (BuildContext context) => ShowPublication(category : 'Domestic Journal'))
+            );
+          },
+          title: Text('Domestic Journal', textScaleFactor: 1.3),
+        ),
+      ),
+    ],
+  );
 }
