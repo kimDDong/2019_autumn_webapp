a5461ced0e381b869186398f9e486ac6f7a992db
jonghwi Wang <wangjh789@daum.net>
2019-11-12 01:56:55 +0900

    d

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/wangjonghwi/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/wangjonghwi/Workspace/2019_autumn_webapp"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/gohdong/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gohdong/IdeaProjects/selab"
+export "FLUTTER_TARGET=/Users/gohdong/IdeaProjects/selab/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/wangjonghwi/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/gohdong/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
